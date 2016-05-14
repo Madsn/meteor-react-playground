1f2de43d30be3f9a0fdf067e5c37b993ee8f8dba
Madsn <m@madsn.net>
2016-05-15 00:18:38 +0200

    add bootstrap less

--- imports/ui/navbar/Navbar.jsx
+++ imports/ui/navbar/Navbar.jsx
@@ -1,9 +1,29 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router';
 
 export default class Navbar extends Component {
   render() {
     return (
-      <div id="navbar"></div>
+      <div className="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div className="container-fluid">
+          <div className="container">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <Link className="navbar-brand" to="/"><i className="glyphicon glyphicon-envelope"></i></Link>
+            </div>
+            <div className="navbar-collapse collapse navbar-responsive-collapse">
+              <ul className="nav navbar-nav">
+                <li><Link to="/">Main</Link></li>
+                <li><Link to="/todo">Todo</Link></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
